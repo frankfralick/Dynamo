62d1152a478dac5f32da161b9e6b68dfbe610db1
unknown <frankfralick@2FDVKV1.beckgroup.com>
2013-11-14 23:02:26 -0500

    Started adding Dynamo related dependencies and fields.

--- src/DynamoInventor/DynamoAddinButton.cs
+++ src/DynamoInventor/DynamoAddinButton.cs
@@ -1,23 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using Inventor;
+
+using Dynamo;
+using Dynamo.Controls;
+using Dynamo.FSchemeInterop;
+using Dynamo.Utilities;
 
 namespace DynamoInventor
 {
-    internal class DynamoAddinButton : Button
+    internal class DynamoInventorAddinButton : Button
     {
-		public DynamoAddinButton(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, Icon standardIcon, Icon largeIcon, ButtonDisplayEnum buttonDisplayType)
+
+        //Dynamo
+        public static ExecutionEnvironment env;
+        private static DynamoView dynamoView;
+        private DynamoController dynamoController;
+        private static bool isRunning = false;
+        public static double? dynamoViewX = null;
+        public static double? dynamoViewY = null;
+        public static double? dynamoViewWidth = null;
+        public static double? dynamoViewHeight = null;
+        private bool handledCrash = false;
+
+		public DynamoInventorAddinButton(string displayName, 
+                                         string internalName, 
+                                         CommandTypesEnum commandType, 
+                                         string clientId, 
+                                         string description, 
+                                         string tooltip, 
+                                         Icon standardIcon, 
+                                         Icon largeIcon, 
+                                         ButtonDisplayEnum buttonDisplayType)
             : base(displayName, internalName, commandType, clientId, description, tooltip, standardIcon, largeIcon, buttonDisplayType)
 		{		
 		}
 
-		public DynamoAddinButton(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, ButtonDisplayEnum buttonDisplayType)
+		public DynamoInventorAddinButton(string displayName, 
+                                         string internalName, 
+                                         CommandTypesEnum commandType, 
+                                         string clientId, 
+                                         string description, 
+                                         string tooltip, 
+                                         ButtonDisplayEnum buttonDisplayType)
 			: base(displayName, internalName, commandType, clientId, description, tooltip, buttonDisplayType)
 		{		
 		}
@@ -31,6 +62,13 @@ namespace DynamoInventor
 				if (InventorApplication.ActiveEditObject is AssemblyDocument)
 				{
 					//Start Dynamo!  
+                    //get window handle
+                    IntPtr mwHandle = Process.GetCurrentProcess().MainWindowHandle;
+
+                    string versionYear = InventorApplication.SoftwareVersion.DisplayVersion;
+
+                    env = new ExecutionEnvironment();
+
 				}
 				else
 				{
