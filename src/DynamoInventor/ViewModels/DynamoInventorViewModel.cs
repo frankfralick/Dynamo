db7c7609ddb4f0a8be7c65bd5bc57d1ddb42cdfd
frankfralick <frankfralick@gmail.com>
2013-11-15 15:06:43 -0500

    Added context strings for Inventor 2013 and 2014 to Dynamo.Context.  Work on making stubs that will allow Dynamo to initialize from Inventor.

--- src/DynamoInventor/ViewModels/DynamoInventorViewModel.cs
+++ src/DynamoInventor/ViewModels/DynamoInventorViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Dynamo;
 using Dynamo.Nodes;
 using Dynamo.ViewModels;
 
@@ -10,5 +11,66 @@ namespace DynamoInventor.ViewModels
 {
     class DynamoInventorViewModel : DynamoViewModel    
     {
+        public DynamoInventorViewModel(DynamoController controller, string commandFilePath) : base(controller, commandFilePath) { }
+
+        public override bool CanRunDynamically
+        {
+            get
+            {
+                //we don't want to be able to run
+                //dynamically if we're in debug mode
+                bool manTran = true;
+                return !manTran && !debug;
+            }
+            set
+            {
+                canRunDynamically = value;
+                RaisePropertyChanged("CanRunDynamically");
+            }
+        }
+
+        public override bool DynamicRunEnabled
+        {
+            get
+            {
+                return dynamicRun;
+            }
+            set
+            {
+                dynamicRun = value;
+                RaisePropertyChanged("DynamicRunEnabled");
+            }
+        }
+
+        public override bool RunInDebug
+        {
+            get { return debug; }
+            set
+            {
+                debug = value;
+
+                //toggle off dynamic run
+                CanRunDynamically = !debug;
+
+                if (debug)
+                    DynamicRunEnabled = false;
+
+                RaisePropertyChanged("RunInDebug");
+            }
+        }
+
+        //public override Function CreateFunction(
+        //    IEnumerable<string> inputs,
+        //    IEnumerable<string> outputs,
+        //    FunctionDefinition functionDefinition)
+        //{
+        //    if (functionDefinition.WorkspaceModel.Nodes.Any(x => x is RevitTransactionNode)
+        //        || functionDefinition.Dependencies.Any(d => d.WorkspaceModel.Nodes.Any(x => x is RevitTransactionNode)))
+        //    {
+        //        //return new FunctionWithRevit(inputs, outputs, functionDefinition);
+        //    }
+        //    return base.CreateFunction(inputs, outputs, functionDefinition);
+        //}
+
     }
 }
