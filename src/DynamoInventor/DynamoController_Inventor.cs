8fab40260d3a2fa1d8c09918e511b43c7e37fa0e
frankfralick <frankfralick@gmail.com>
2013-11-14 23:45:03 -0500

    Added DynamoInventorViewModel and DynamoInventor_Controller.

---
+++ src/DynamoInventor/DynamoController_Inventor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Dynamo;
+
+namespace Dynamo
+{
+    class DynamoController_Inventor : DynamoController
+    {
+
+    }
+}
