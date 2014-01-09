f6ca66f5ddc2b46b88ab3ce39f3ac5911e2dae64
frankfralick <frankfralick@gmail.com>
2014-01-09 14:33:44 -0500

    Added DSAssemblyComponent node stub.

---
+++ src/DSInventorNodes/ComponentPlacement/DSAssemblyComponent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Inventor;
+
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Interfaces;
+using DSNodeServices;
+using Dynamo.Models;
+using Dynamo.Utilities;
+using DSInventorNodes.GeometryConversion;
+using InventorServices.Persistence;
+using Point = Autodesk.DesignScript.Geometry.Point;
+
+namespace DSInventorNodes.ComponentPlacement
+{
+    [RegisterForTrace]
+    public class DSAssemblyComponent
+    {
+
+        #region Internal properties
+        internal WorkPoint InternalWorkPoint { get; private set; }
+
+        //public override ComponentOccurrence InternalOccurrence
+        //{
+        //    get { return InternalWorkPoint.; }
+        //}
+        
+        #endregion
+
+        #region Private constructors
+        private DSAssemblyComponent()
+        {
+        }
+
+        #endregion
+
+        #region Private mutators
+
+        #endregion
+
+        #region Public properties
+
+        #endregion
+
+        #region Public static constructors
+        public static DSAssemblyComponent ByConstraints(List<Point> points)
+        {
+        }
+
+        #endregion
+
+        #region Internal static constructors
+        #endregion
+
+        #region Tesselation
+
+        #endregion
+    
+    }
+}
