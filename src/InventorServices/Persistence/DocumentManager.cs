e17e20957b87a67d853f792599b212397a937d87
unknown <frankfralick@2FDVKV1.beckgroup.com>
2014-01-11 18:09:45 -0500

    Added InventorApplication to InventorServices.Persistence.DocumentManager.

--- src/InventorServices/Persistence/DocumentManager.cs
+++ src/InventorServices/Persistence/DocumentManager.cs
@@ -12,13 +12,14 @@ namespace InventorServices.Persistence
     /// <summary>
     /// This class holds static references that the application needs.  
     /// </summary>
-    /// 
-    //TODO: This should probably not be public, but would need to be marked as friend to DSInventorNodes.
     public class DocumentManager
     {
-        public static Inventor.Application InventorApplication { get; set; }
+        //TODO Dispose of this!
+        private ApprenticeServerComponentClass apprenticeServer;
 
         public static AssemblyDocument ActiveAssemblyDoc { get; set; }
+
+        public static Inventor.Application InventorApplication { get; set; }
 
         public ApprenticeServerComponent ActiveApprenticeServer
         {
@@ -33,12 +34,14 @@ namespace InventorServices.Persistence
 
             set { value = apprenticeServer; }
         }
-
+                
         //This is the name of the storage for Dynamo object bindings.
         private static string dynamoStorageName = "Dynamo";
+
         public static string DynamoStorageName
         {
             get { return dynamoStorageName; }
         }
+
     }
 }
