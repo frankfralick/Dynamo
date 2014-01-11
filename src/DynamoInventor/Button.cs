2b7e9efb5bf9c1a084c6158e46764052f20a74ac
unknown <frankfralick@2FDVKV1.beckgroup.com>
2014-01-11 18:11:35 -0500

    Button now gets application reference from InventorServices.

--- src/DynamoInventor/Button.cs
+++ src/DynamoInventor/Button.cs
@@ -4,63 +4,59 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Inventor;
+
+using InventorServices.Persistence;
 
 namespace DynamoInventor
 {
     internal abstract class Button
     {
-		#region Data Members
-
-		//private data members:
-		private static Inventor.Application m_inventorApplication;
-
-		private ButtonDefinition m_buttonDefinition;
+		#region Private fields		
+		private ButtonDefinition buttonDefinition;
 
         private ButtonDefinitionSink_OnExecuteEventHandler ButtonDefinition_OnExecuteEventDelegate;
-
 		#endregion		
 
-		#region "Properties"
-		
-		public static Inventor.Application InventorApplication
-		{
-			set { m_inventorApplication = value; }
-			get { return m_inventorApplication; }
-        }
-
+		#region Public properties
 		public Inventor.ButtonDefinition ButtonDefinition
 		{
-			get { return m_buttonDefinition; }
+			get { return buttonDefinition; }
 		}
 
 		#endregion
         
-		#region "Methods"
+		#region Public constructors
 
-		public Button(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, Icon standardIcon, Icon largeIcon, ButtonDisplayEnum buttonDisplayType)
+		public Button(string displayName, 
+                      string internalName, 
+                      CommandTypesEnum commandType, 
+                      string clientId, 
+                      string description, 
+                      string tooltip, 
+                      Icon standardIcon, 
+                      Icon largeIcon, 
+                      ButtonDisplayEnum buttonDisplayType)
 		{
 			try
 			{
-				//get IPictureDisp for icons
-				stdole.IPictureDisp standardIconIPictureDisp;
-				
-				//standardIconIPictureDisp = (stdole.IPictureDisp)Support.IconToIPicture(standardIcon);
-				standardIconIPictureDisp = PictureDispConverter.ToIPictureDisp(standardIcon);
-				
+				stdole.IPictureDisp standardIconIPictureDisp;				
+				standardIconIPictureDisp = PictureDispConverter.ToIPictureDisp(standardIcon);			
 				stdole.IPictureDisp largeIconIPictureDisp;
-                //largeIconIPictureDisp = (stdole.IPictureDisp)Support.IconToIPicture(largeIcon);
-		        largeIconIPictureDisp = PictureDispConverter.ToIPictureDisp(largeIcon);
-				//create button definition
-				m_buttonDefinition = m_inventorApplication.CommandManager.ControlDefinitions.AddButtonDefinition(displayName, internalName, commandType, clientId, description, tooltip, standardIconIPictureDisp , largeIconIPictureDisp, buttonDisplayType);
+		        largeIconIPictureDisp = PictureDispConverter.ToIPictureDisp(largeIcon);   
+				buttonDefinition = DocumentManager.InventorApplication.CommandManager.ControlDefinitions.AddButtonDefinition(displayName, 
+                                                                                                                             internalName, 
+                                                                                                                             commandType, 
+                                                                                                                             clientId, 
+                                                                                                                             description, 
+                                                                                                                             tooltip, 
+                                                                                                                             standardIconIPictureDisp , 
+                                                                                                                             largeIconIPictureDisp, 
+                                                                                                                             buttonDisplayType);
 												
-				//enable the button
-                m_buttonDefinition.Enabled = true;
-				
-				//connect the button event sink
+                buttonDefinition.Enabled = true;			
                 ButtonDefinition_OnExecuteEventDelegate = new ButtonDefinitionSink_OnExecuteEventHandler(ButtonDefinition_OnExecute);
-                m_buttonDefinition.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
+                buttonDefinition.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
 			}
 
 			catch(Exception e)
@@ -69,20 +65,31 @@ namespace DynamoInventor
 			}
 		}
 
-		public Button(string displayName, string internalName, CommandTypesEnum commandType, string clientId, string description, string tooltip, ButtonDisplayEnum buttonDisplayType)
+		public Button(string displayName, 
+                      string internalName, 
+                      CommandTypesEnum commandType, 
+                      string clientId, 
+                      string description, 
+                      string tooltip, 
+                      ButtonDisplayEnum buttonDisplayType)
 		{
 			try
 			{			
-				//create button definition
-                m_buttonDefinition = m_inventorApplication.CommandManager.ControlDefinitions.AddButtonDefinition(displayName, internalName, commandType, clientId, description, tooltip, Type.Missing, Type.Missing, buttonDisplayType);
+                buttonDefinition = DocumentManager.InventorApplication.CommandManager.ControlDefinitions.AddButtonDefinition(displayName, 
+                                                                                                                             internalName, 
+                                                                                                                             commandType, 
+                                                                                                                             clientId, 
+                                                                                                                             description, 
+                                                                                                                             tooltip, 
+                                                                                                                             Type.Missing, 
+                                                                                                                             Type.Missing, 
+                                                                                                                             buttonDisplayType);
 								
-				//enable the button
-                m_buttonDefinition.Enabled = true;
-				
-				//connect the button event sink
+                buttonDefinition.Enabled = true;
 				ButtonDefinition_OnExecuteEventDelegate = new ButtonDefinitionSink_OnExecuteEventHandler(ButtonDefinition_OnExecute);
-                m_buttonDefinition.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
+                buttonDefinition.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
 			}
+
 			catch(Exception e)
 			{
                 throw new ApplicationException(e.ToString());
