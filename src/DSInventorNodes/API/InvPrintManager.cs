85c52c318601687edc19c4fc394096c8c104ca8a
frankfralick <frankfralick@gmail.com>
2014-01-17 15:59:39 -0500

    Add InvPrintManager

---
+++ src/DSInventorNodes/API/InvPrintManager.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Inventor;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.DesignScript.Interfaces;
+using DSNodeServices;
+using Dynamo.Models;
+using Dynamo.Utilities;
+using DSInventorNodes.GeometryConversion;
+using InventorServices.Persistence;
+
+namespace DSInventorNodes
+{
+    [RegisterForTrace]
+    public class InvPrintManager
+    {
+        #region Internal properties
+        internal Inventor.PrintManager InternalPrintManager { get; set; }
+
+        internal Object InternalApplication
+        {
+            get { return PrintManagerInstance.Application; }
+        }
+
+        internal InvObjectTypeEnum InternalType
+        {
+            get { return InvObjectTypeEnum.ByInvObjectTypeEnum(PrintManagerInstance.Type); }
+        }
+
+
+        internal PrintColorModeEnum InternalColorMode { get; set; }
+
+        internal int InternalNumberOfCopies { get; set; }
+
+        internal PrintOrientationEnum InternalOrientation { get; set; }
+
+        internal double InternalPaperHeight { get; set; }
+
+        internal PaperSizeEnum InternalPaperSize { get; set; }
+
+        internal int InternalPaperSource { get; set; }
+
+        internal double InternalPaperWidth { get; set; }
+
+        internal string InternalPrinter { get; set; }
+
+        internal int InternalPrinterDeviceContext { get; set; }
+        #endregion
+
+        internal PrintManager InternalkNoOwnership
+        {
+            get { return Inventor.PrintManager.kNoOwnership; }
+        }
+        internal PrintManager InternalkSaveOwnership
+        {
+            get { return Inventor.PrintManager.kSaveOwnership; }
+        }
+        internal PrintManager InternalkExclusiveOwnership
+        {
+            get { return Inventor.PrintManager.kExclusiveOwnership; }
+        }
+        #region Private constructors
+        private InvPrintManager(InvPrintManager invPrintManager)
+        {
+            InternalPrintManager = invPrintManager.InternalPrintManager;
+        }
+
+        private InvPrintManager(Inventor.PrintManager invPrintManager)
+        {
+            InternalPrintManager = invPrintManager;
+        }
+        #endregion
+
+        #region Private methods
+        private void InternalPrintToFile(string fileName)
+        {
+            PrintManagerInstance.PrintToFile( fileName);
+        }
+
+        private void InternalSubmitPrint()
+        {
+            PrintManagerInstance.SubmitPrint();
+        }
+
+        #endregion
+
+        #region Public properties
+        public Inventor.PrintManager PrintManagerInstance
+        {
+            get { return InternalPrintManager; }
+            set { InternalPrintManager = value; }
+        }
+
+        public Object Application
+        {
+            get { return InternalApplication; }
+        }
+
+        public InvObjectTypeEnum Type
+        {
+            get { return InternalType; }
+        }
+
+        public InvPrintColorModeEnum ColorMode
+        {
+            get { return InternalColorMode; }
+            set { InternalColorMode = value; }
+        }
+
+        public Invint NumberOfCopies
+        {
+            get { return InternalNumberOfCopies; }
+            set { InternalNumberOfCopies = value; }
+        }
+
+        public InvPrintOrientationEnum Orientation
+        {
+            get { return InternalOrientation; }
+            set { InternalOrientation = value; }
+        }
+
+        public Invdouble PaperHeight
+        {
+            get { return InternalPaperHeight; }
+            set { InternalPaperHeight = value; }
+        }
+
+        public InvPaperSizeEnum PaperSize
+        {
+            get { return InternalPaperSize; }
+            set { InternalPaperSize = value; }
+        }
+
+        public Invint PaperSource
+        {
+            get { return InternalPaperSource; }
+            set { InternalPaperSource = value; }
+        }
+
+        public Invdouble PaperWidth
+        {
+            get { return InternalPaperWidth; }
+            set { InternalPaperWidth = value; }
+        }
+
+        public Invstring Printer
+        {
+            get { return InternalPrinter; }
+            set { InternalPrinter = value; }
+        }
+
+        public Invint PrinterDeviceContext
+        {
+            get { return InternalPrinterDeviceContext; }
+            set { InternalPrinterDeviceContext = value; }
+        }
+
+        #endregion
+        public PrintManager kNoOwnership
+        {
+            get { return InternalkNoOwnership; }
+        }
+        public PrintManager kSaveOwnership
+        {
+            get { return InternalkSaveOwnership; }
+        }
+        public PrintManager kExclusiveOwnership
+        {
+            get { return InternalkExclusiveOwnership; }
+        }
+        #region Public static constructors
+        public static InvPrintManager ByInvPrintManager(InvPrintManager invPrintManager)
+        {
+            return new InvPrintManager(invPrintManager);
+        }
+        public static InvPrintManager ByInvPrintManager(Inventor.PrintManager invPrintManager)
+        {
+            return new InvPrintManager(invPrintManager);
+        }
+        #endregion
+
+        #region Public methods
+        public void PrintToFile(string fileName)
+        {
+            InternalPrintToFile( fileName);
+        }
+
+        public void SubmitPrint()
+        {
+            InternalSubmitPrint();
+        }
+
+        #endregion
+    }
+}
