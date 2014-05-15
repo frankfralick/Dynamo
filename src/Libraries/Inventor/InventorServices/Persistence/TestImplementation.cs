498eccf07a573a3a9e498b761351b3e3c85204e2
frankfralick <frankfralick@gmail.com>
2014-05-15 14:01:06 -0400

    Testing SimpleInjector

---
+++ src/Libraries/Inventor/InventorServices/Persistence/TestImplementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorServices.Persistence
+{
+    class TestImplementation : ITestInterface
+    {
+        string time = "party time";
+
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                value = time;
+            }
+        }
+
+        public TestImplementation()
+        {
+
+        }
+    }
+}
---
+++ src/Libraries/Inventor/InventorServices/Persistence/TestUserClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorServices.Persistence
+{
+    public class TestUserClass
+    {
+        ITestInterface _testInterface;
+
+        public TestUserClass(ITestInterface testInterface)
+        {
+            _testInterface = testInterface;
+        }
+    }
+}
