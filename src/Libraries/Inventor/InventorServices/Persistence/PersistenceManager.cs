4be4a7f7d4e7b684db73e2bd64f256c1fb690ed6
frankfralick <frankfralick@gmail.com>
2014-05-15 14:04:55 -0400

    Rafactor

--- src/Libraries/Inventor/InventorServices/Persistence/PersistenceManager.cs
+++ src/Libraries/Inventor/InventorServices/Persistence/PersistenceManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Linq;
 using System.Text;
+using SimpleInjector;
 
 using Inventor;
 
@@ -12,14 +13,26 @@ namespace InventorServices.Persistence
     /// <summary>
     /// This class holds static references that the application needs.  
     /// </summary>
-    public class InventorPersistenceManager
+    public class PersistenceManager
     {
+        //public InventorPersistenceManager()
+        //{
+        //}
         //TODO Dispose of this!
         private static ApprenticeServerComponentClass apprenticeServer;
 
         public static AssemblyDocument ActiveAssemblyDoc { get; set; }
 
         public static PartDocument ActivePartDoc { get; set; }
+
+        public static Document ActiveDocument
+        {
+            get
+            {
+                return InventorApplication.ActiveDocument;
+            }
+        }
+        
 
         public static Inventor.Application InventorApplication { get; set; }
 
@@ -45,5 +58,19 @@ namespace InventorServices.Persistence
             get { return dynamoStorageName; }
         }
 
+        public static Container IoC { get; set; }
+
+        public static void LetThereBeIoC()
+        {
+            Container container = new Container();
+            IoC = container;
+            IoC.Register<ITestInterface, TestImplementation>(Lifestyle.Transient);
+            IoC.Register<IObjectBinder, ModuleReferenceKeyBinder>(Lifestyle.Transient);
+            IoC.Register<IContextArray, ModuleContextArray>(Lifestyle.Transient);
+            //Implementations of ISerializableIdManager need a second contstructor for the serialization engine to call,
+            //so this registration needs to have a delegate to the default constructor so SimpleInjector knows what to do.
+            IoC.Register<ISerializableIdManager<List<Tuple<string, int, int, byte[]>>>>(() => new ModuleIdManager());
+            IoC.Register<IContextManager, ModuleContextManager>();
+        }
     }
 }
