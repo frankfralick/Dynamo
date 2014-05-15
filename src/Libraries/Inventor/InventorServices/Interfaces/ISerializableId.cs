8382a1a32ffb66108edc85c6a040b161343263bd
frankfralick <frankfralick@gmail.com>
2014-05-15 13:59:53 -0400

    Add ISerializableId

---
+++ src/Libraries/Inventor/InventorServices/Interfaces/ISerializableId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace InventorServices
+{
+    interface ISerializableId<T>
+    {
+        T Key { get; set; }
+    }
+}
