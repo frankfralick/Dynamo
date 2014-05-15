ffb227f8366bf87ab65b41504cc0e59a3ef6b335
frankfralick <frankfralick@gmail.com>
2014-05-15 14:04:01 -0400

    Add ModuleContextArray

---
+++ src/Libraries/Inventor/InventorServices/Persistence/ModuleContextArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorServices.Persistence
+{
+    public class ModuleContextArray : IContextArray
+    {
+        public Tuple<int, int> Context
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
