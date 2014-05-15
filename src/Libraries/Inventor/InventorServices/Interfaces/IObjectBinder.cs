359ea83cafbfefdea0aa6c88d02ac5051f0fd030
frankfralick <frankfralick@gmail.com>
2014-05-15 13:52:00 -0400

    Add IObjectBinder

---
+++ src/Libraries/Inventor/InventorServices/Interfaces/IObjectBinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorServices.Persistence
+{
+    public interface IObjectBinder
+    {
+        IObjectKey<T> GetObjectKey<T>();
+        bool GetObjectFromTrace<T>(out T e);
+        void SetObjectForTrace<T>();
+    }
+}
