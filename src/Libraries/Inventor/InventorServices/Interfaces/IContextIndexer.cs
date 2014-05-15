2d4f2c7c0661a278f67406b0df70f346402d2e52
frankfralick <frankfralick@gmail.com>
2014-05-15 14:00:16 -0400

    Add IContextIndexer

---
+++ src/Libraries/Inventor/InventorServices/Interfaces/IContextIndexer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InventorServices.Persistence
+{
+    public interface IContextIndexer
+    {
+        IContextArray ContextArray { get; set; }
+    }
+}
