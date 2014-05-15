fbafe0c6bea9a02f7f37e63ecd489b6315f8ec7a
frankfralick <frankfralick@gmail.com>
2014-05-15 13:52:44 -0400

    Add ISerializableIdManager

---
+++ src/Libraries/Inventor/InventorServices/Interfaces/ISerializableIdManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace InventorServices.Persistence
+{
+    public interface ISerializableIdManager<T>
+    {
+        T Id { get; set; }
+        void GetObjectData(SerializationInfo info, StreamingContext context);
+    }
+}
