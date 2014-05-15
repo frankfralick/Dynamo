0cd5e54be17ca2141d554761edfdc795a5fc77be
frankfralick <frankfralick@gmail.com>
2014-05-15 14:03:34 -0400

    Add ModuleContextManager

---
+++ src/Libraries/Inventor/InventorServices/Persistence/ModuleContextManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace InventorServices.Persistence
+{
+    public class ModuleContextManager : IContextManager
+    {
+        ReferenceManager refManager;
+
+        dynamic IContextManager.BindingContextManager
+        {
+            get 
+            { 
+                return PersistenceManager.ActiveDocument; 
+            }
+        }
+    }
+}
