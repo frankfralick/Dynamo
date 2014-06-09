bf8876963e4dac9d72abcf8f08c9d979d2a69814
frankfralick <frankfralick@gmail.com>
2014-06-09 10:59:15 -0400

    Start installer

---
+++ tools/install/Extra/DynamoInventorAddinsBackup.bat
@@ -0,0 +1,26 @@
+REM SET Dynamo2013AddinOld="%ProgramData%\Autodesk\Revit\Addins\2013\Dynamo.addin"
+REM SET Dynamo2014AddinOld="%ProgramData%\Autodesk\Revit\Addins\2014\Dynamo.addin"
+REM SET DynamoVasariAddinOld="%ProgramData%\Autodesk\Vasari\Addins\2014\Dynamo.addin"
+SET DynamoInventorAddinOld = "%ProgramData"\Autodesk\Inventor 2014\Addins\DynamoInventor.addin"
+
+REM SET Dynamo2013AddinNew="Dynamo.OLD"
+REM SET Dynamo2014AddinNew="Dynamo.OLD"
+REM SET DynamoVasariAddinNew="Dynamo.OLD"
+
+REM IF EXIST %Dynamo2013AddinOld% (
+REM 	ren %Dynamo2013AddinOld% %Dynamo2013AddinNew%
+REM ) ELSE (
+REM 	echo "No Dynamo 2013 addin found."
+REM )
+
+REM IF EXIST %Dynamo2014AddinOld% (
+REM 	ren %Dynamo2014AddinOld% %Dynamo2014AddinNew%
+REM ) ELSE (
+REM 	echo "No Dynamo 2014 addin found."
+REM )
+
+REM IF EXIST %DynamoVasariAddinOld% (
+REM 	ren %DynamoVasariAddinOld% %DynamoVasariAddinNew%
+REM ) ELSE (
+REM 	echo "No Dynamo Vasari 2014 addin found."
+REM )
